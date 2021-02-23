ccf1f1d394053bfff509be4a10808b1d006f26a0
LaisLimaSantos <laissp25@gmail.com>
2021-02-22 22:01:26 -0300

    rotas de entrar e cadastrar

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+
+export class Tema {
+
+    public id: number
+    public titulo: string
+    public texto: string
+    public data: Date
+    public usuario: User
+}
